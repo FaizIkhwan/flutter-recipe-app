e4ba43980a3ebffdf4bc6a76f3fb3ceb47f4afb6
Faiz Ikhwan <faizikhwantop@gmail.com>
2021-05-29 22:24:05 +0800

    [] init commit

---
+++ recipe_app/android/app/src/main/kotlin/com/faizikhwan/recipe_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.faizikhwan.recipe_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ recipe_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Recipe App",
+      debugShowCheckedModeBanner: false,
+      home: Text("hello world"),
+    );
+  }
+}
